363d3a5ca8e5fad5c44f34fa46010c8c4272d856
AbhijitKumarPandey <156564538+AbhijitKumarPandey@users.noreply.github.com>
2024-03-28 21:57:48 +0530

    Add files via upload

---
+++ Swapping_Use_third_variable.java
@@ -0,0 +1,17 @@
+
+public class Swapping_Use_third_variable {
+	public static void main(String[] args) {
+		int a = 12;
+		int b = 13;
+		int c;
+		System.out.println("Before Swapping");
+		System.out.println(a + " " + b);
+		c = a;
+		a = b;
+		b = c;
+		System.out.println("After Swapping");
+		System.out.println(a + " " + b);
+		
+	}
+
+}
